353827f298dbc84aa6637d464a29f733360c0549
arnaud.notter <arnaud.notter@epita.fr>
2023-08-14 00:31:43 +0200

    :zap: start to interact with api rugby

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import UserRoute from './user.route';
 import GroupRoute from './group.route';
+import rugbyApiRepository from '../repositories/rugby-api.repository';
 
 // Import route
 
@@ -10,5 +11,6 @@ export const router = express.Router();
 // Use route
 router.use('/user', UserRoute);
 router.use('/group', GroupRoute);
+router.get('/matches', rugbyApiRepository.fetchMatches);
 
 export default router;
